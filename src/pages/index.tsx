f11df4bbfeb4a9f5b44003c2a93c0574d3c605bb
Max Cheng <ofcoursemax@gmail.com>
2023-05-03 12:31:25 -0700

    feat: finish left part design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,51 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
-import {Button, Col, Modal, Row} from "antd";
+import {Button, Col, Modal, Row, Space, Typography} from "antd";
 import {useState} from "react";
 import {CloseOutlined} from '@ant-design/icons';
 
 const inter = Inter({subsets: ['latin']})
+
+
+
+const LeftPart = () => {
+    const {Title, Text} = Typography;
+    return (
+        <div>
+                <Row>
+                    <Col span={24}>
+                        <Title level={4}>CONGRATS!</Title>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Title level={2}>
+                            Your watch sold!
+                        </Title>
+                    </Col>
+                    <Col span={24}>
+                        <Text type="secondary">
+                            You have 1 business day to accept the sale.<br/>
+                            If you do not accept, it will be automatically rejected.
+                        </Text>
+                    </Col>
+                </Row>
+                <Row justify={"center"} gutter={[12,20]}>
+                    <Col span={24}>
+                        <Button type={"primary"} shape={"round"} block={true} height={"15px"}>
+                            Accept sale
+                        </Button>
+                    </Col>
+
+                    <Col span={24}>
+                        <Button type={"text"} shape={"round"} block={true}>
+                            Reject sale
+                        </Button>
+                    </Col>
+                </Row>
+        </div>
+    )
+}
 
 export default function Home() {
     const [open, setOpen] = useState(false);
@@ -32,10 +73,11 @@ export default function Home() {
             <Modal
                 title={null}
                 footer={null}
+                width={"50%"}
                 open={open}
                 closable={false}
-                okButtonProps={{ disabled: true }}
-                cancelButtonProps={{ disabled: true }}
+                okButtonProps={{disabled: true}}
+                cancelButtonProps={{disabled: true}}
             >
                 <Row
                     justify={"end"}
@@ -44,7 +86,7 @@ export default function Home() {
                         <Button
                             type="text"
                             shape="circle"
-                            icon={<CloseOutlined />}
+                            icon={<CloseOutlined/>}
                             onClick={(e) => handleCancel(e)}
                         />
                     </Col>
@@ -53,7 +95,7 @@ export default function Home() {
                     <Col
                         span={12}
                     >
-                        left
+                        <LeftPart/>
                     </Col>
                     <Col
                         span={12}
