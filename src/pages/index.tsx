d9faf694db33a20603f391999280f6a6169c678f
Max Cheng <ofcoursemax@gmail.com>
2023-05-03 12:35:33 -0700

    feat: abstract the button design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,43 +6,59 @@ import {CloseOutlined} from '@ant-design/icons';
 
 const inter = Inter({subsets: ['latin']})
 
-
+const BlockButton = (props: any) => {
+    return (
+        <Button
+            style={{height:"56px", borderRadius:"18px"}}
+            type={props?.type}
+            block={true}
+            onClick={props?.onClick}
+            {...props}
+        >
+            {props.text}
+        </Button>
+    )
+}
 
 const LeftPart = () => {
     const {Title, Text} = Typography;
+
+
     return (
         <div>
-                <Row>
-                    <Col span={24}>
-                        <Title level={4}>CONGRATS!</Title>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}>
-                        <Title level={2}>
-                            Your watch sold!
-                        </Title>
-                    </Col>
-                    <Col span={24}>
-                        <Text type="secondary">
-                            You have 1 business day to accept the sale.<br/>
-                            If you do not accept, it will be automatically rejected.
-                        </Text>
-                    </Col>
-                </Row>
-                <Row justify={"center"} gutter={[12,20]}>
-                    <Col span={24}>
-                        <Button type={"primary"} shape={"round"} block={true} height={"15px"}>
-                            Accept sale
-                        </Button>
-                    </Col>
+            <Row>
+                <Col span={24}>
+                    <Text>CONGRATS!</Text>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <Title level={2}>
+                        Your watch sold!
+                    </Title>
+                </Col>
+                <Col span={24}>
+                    <Text type="secondary">
+                        You have 1 business day to accept the sale.<br/>
+                        If you do not accept, it will be automatically rejected.
+                    </Text>
+                </Col>
+            </Row>
+            <Row justify={"center"} gutter={[12, 10]}>
+                <Col span={24}>
+                    <BlockButton
+                        type={"primary"}
+                        text={"Accept sale"}
+                    />
+                </Col>
 
-                    <Col span={24}>
-                        <Button type={"text"} shape={"round"} block={true}>
-                            Reject sale
-                        </Button>
-                    </Col>
-                </Row>
+                <Col span={24}>
+                    <BlockButton
+                        type={"text"}
+                        text={"Reject sale"}
+                    />
+                </Col>
+            </Row>
         </div>
     )
 }
