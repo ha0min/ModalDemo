76fa217dda4c24a13dcdea8bc1d43f4e0efaf1d6
Max Cheng <ofcoursemax@gmail.com>
2023-05-05 12:17:03 -0700

    feat: add preload data modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
-import {Button, message, ConfigProvider, Layout, Space, Input} from 'antd';
-import React, {useState} from 'react';
+import {Button, message, ConfigProvider, Layout, Space, Input, Spin, Divider, Row, Col} from 'antd';
+import React, {useEffect, useState} from 'react';
 import {useDecision, useOrder} from "@/utils/common";
 import {OrderData} from "@/compiler/types";
 import {OrderModal} from "@/components/order-modal";
 import {Typography} from "antd";
 import {primaryColor, secondaryColor} from "@/styles/colors";
 import {BlockButton} from "@/components/common/block-button";
+import {ProCard} from "@ant-design/pro-components";
 
-const {Title,Text} = Typography;
+const {Title, Text} = Typography;
 
 const {Header, Content, Footer} = Layout;
 
-export default function Home() {
+const DataLoadEveryTimeModal = () => {
     const [open, setOpen] = useState(false);
     const [orderData, setOrderData] = useState<OrderData | null>(null);
     const {isOrderMutating, isOrderError, orderTrigger, orderReset} = useOrder();
@@ -33,7 +34,7 @@ export default function Home() {
                 } = order.listing;
 
                 const extractedData = {
-                    requestId: '123',
+                    requestId: inputId,
                     brandName: model.brand.displayName,
                     modelName: model.displayName,
                     manufactureYear,
@@ -107,6 +108,160 @@ export default function Home() {
 
 
     return (
+        <div>
+            <Space direction="vertical" size="large">
+                <Text>Input the order id(endpoint id): </Text>
+                <Input
+                    placeholder="Basic usage"
+                    defaultValue={'123'}
+                    onChange={(e) => setInputId(e.target.value)}
+                />
+                <BlockButton
+                    type='primary'
+                    onClick={showModal}
+                    text={'Open Modal'}
+                />
+            </Space>
+
+            <OrderModal
+                open={open}
+                orderData={orderData}
+                onAcceptClick={onAcceptClick}
+                onDeclineClick={onDeclineClick}
+                isAcceptPosting={isAcceptPosting}
+                isRejectPosting={isRejectPosting}
+                isOrderError={isOrderError}
+                isOrderMutating={isOrderMutating}
+                onModalClose={() => setClose()}
+            />
+        </div>
+    )
+}
+
+const DataPreloadModal = () => {
+    const [open, setOpen] = useState(false);
+    const [orderData, setOrderData] = useState<OrderData | null>(null);
+    const {isOrderMutating, isOrderError, orderTrigger, orderReset} = useOrder();
+    const {isDecisionMutating, decisionTrigger, decisionReset} = useDecision();
+    const [isAcceptPosting, setIsAcceptPosting] = useState(false);
+    const [isRejectPosting, setIsRejectPosting] = useState(false);
+
+    const inputId = '123';
+    const loadData = () => {
+        orderTrigger({id: inputId})
+            .then((order) => {
+                console.log(order);
+
+                const {
+                    model,
+                    manufactureYear,
+                    condition,
+                    images,
+                } = order.listing;
+
+                const extractedData = {
+                    requestId: inputId,
+                    brandName: model.brand.displayName,
+                    modelName: model.displayName,
+                    manufactureYear,
+                    condition,
+                    imageUrl: images[0].image.url,
+                    salePriceCents: order.salePriceCents,
+                    commissionRateBips: order.commissionRateBips,
+                    sellerFeeCents: order.sellerFeeCents,
+                    payoutAmountCents: order.payoutAmountCents,
+                };
+
+                console.log('extractedData', extractedData);
+
+                setOrderData(extractedData);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    const showModal = () => {
+        setOpen(true);
+    };
+
+    const setClose = () => {
+        setOpen(false);
+    }
+
+    const onAcceptClick = (e: React.MouseEvent<HTMLElement>) => {
+        console.log('handleOk', e);
+        setIsAcceptPosting(true);
+        decisionTrigger({id: inputId, decision: 'accept'})
+            .then((res) => {
+                    console.log('res', res);
+
+                    message.success('Order accepted successfully.');
+                }
+            )
+            .catch((err) => {
+                console.log('err', err);
+                message.error('Order accept failed, please try again.');
+            })
+            .finally(() => {
+                setIsAcceptPosting(false)
+                setClose();
+            });
+    }
+
+
+    const onDeclineClick = (e: React.MouseEvent<HTMLElement>) => {
+        console.log('handleOk', e);
+        setIsRejectPosting(true);
+        decisionTrigger({id: inputId, decision: 'decline'})
+            .then((res) => {
+                    console.log('res', res);
+                    message.success('Order declined successfully.');
+                }
+            )
+            .catch((err) => {
+                console.log('err', err);
+                message.error('Order decline failed, please try again.');
+            })
+            .finally(() => {
+                setIsRejectPosting(false);
+                setClose();
+            });
+    }
+
+    useEffect(() => {
+        loadData(); // preload the data
+    }, []);
+
+    return (
+        <div>
+            <Space direction="vertical" size="large">
+                <Text>Preload the order id: 123</Text>
+
+                <BlockButton
+                    type='primary'
+                    onClick={showModal}
+                    text={'Open Data Preloaded Modal'}
+                    disable={isOrderMutating}
+                />
+            </Space>
+            <OrderModal
+                open={open}
+                orderData={orderData}
+                onAcceptClick={onAcceptClick}
+                onDeclineClick={onDeclineClick}
+                isAcceptPosting={isAcceptPosting}
+                isRejectPosting={isRejectPosting}
+                isOrderError={isOrderError}
+                isOrderMutating={isOrderMutating}
+                onModalClose={() => setClose()}
+            />
+        </div>
+    )
+}
+
+export default function Home() {
+    return (
         <ConfigProvider
             theme={{
                 token: {
@@ -130,33 +285,18 @@ export default function Home() {
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}>
-                    <Space direction="vertical" size="large">
-                        <Text>Input the order id(endpoint id): </Text>
-                        <Input
-                            placeholder="Basic usage"
-                            defaultValue={'123'}
-                            onChange={(e) => setInputId(e.target.value)}
-                        />
-                        <BlockButton
-                            type='primary'
-                            onClick={showModal}
-                            text={'Open Modal'}
-                        />
-                    </Space>
+                    <ProCard gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                        <ProCard title={'Data Preload Modal'} colSpan={12} layout="center" bordered>
+                            <DataPreloadModal/>
+
+                        </ProCard>
+                        <ProCard colSpan={12} title={'Data Load Everytime Modal'} layout="center" bordered>
+                            <DataLoadEveryTimeModal/>
+                        </ProCard>
+                    </ProCard>
                 </Content>
                 <Footer style={{textAlign: 'center'}}>Haomin Cheng</Footer>
             </Layout>
-            <OrderModal
-                open={open}
-                orderData={orderData}
-                onAcceptClick={onAcceptClick}
-                onDeclineClick={onDeclineClick}
-                isAcceptPosting={isAcceptPosting}
-                isRejectPosting={isRejectPosting}
-                isOrderError={isOrderError}
-                isOrderMutating={isOrderMutating}
-                onModalClose={() => setClose()}
-            />
         </ConfigProvider>
     )
 }
